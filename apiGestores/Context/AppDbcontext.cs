67aa355bccf35d3696c985e69a72d8ec4e1081c2
javier.ramirezg <javier.ramirezg@pgn.gob.gt>
2023-03-28 09:27:06 -0600

    Agregar archivos de proyecto.

---
+++ apiGestores/Context/AppDbcontext.cs
@@ -0,0 +1,19 @@
+﻿using apiGestores.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiGestores.Context
+{
+    public class AppDbcontext :DbContext
+    {
+        public AppDbcontext(DbContextOptions<AppDbcontext> options): base (options)
+        {
+                
+        }
+        public DbSet<Gestores_Bd> gestores_db { get; set; }
+
+    }
+}
